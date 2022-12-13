76bdaf743525643b239322a5a428922c69ab02fe
jimmayhugh <teensypi@gmail.com>
2022-12-13 08:06:09 -0800

    20221213-1

--- Configuration.h
+++ Configuration.h
@@ -135,7 +135,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Octopus1 20221212-5" //jlm
+#define CUSTOM_MACHINE_NAME "Octopus1 20221213-1" //jlm
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
@@ -1688,8 +1688,8 @@
 // @section geometry
 
 // The size of the printable area
-#define X_BED_SIZE 220 //jlm
-#define Y_BED_SIZE 220 //jlm
+#define X_BED_SIZE 230 //jlm
+#define Y_BED_SIZE 230 //jlm
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
@@ -1697,7 +1697,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 220 //jlm
+#define Z_MAX_POS 230 //jlm
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
 //#define J_MIN_POS 0
